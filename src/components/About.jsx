883b48540f2911014b2c10f07da1748895da640d
Yuvaan Pradhan <yuvi.pradhan2002@gmail.com>
2025-01-02 13:42:10 +0530

    update final2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import Me from '../assets/WhatsApp Image 2023-09-03 at 11.06.34.jpeg'
+import Me from "../assets/WhatsApp Image 2023-09-03 at 11.06.34.jpeg";
 const About = () => {
   return (
-    <div name='about' className="w-full h-screen bg-pink text-gray-300 bg-gradient-to-b from-black to-gray-800">
+    <div
+      name="about"
+      className="w-full h-screen bg-pink text-gray-300 bg-gradient-to-b from-black to-gray-800"
+    >
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
@@ -11,20 +14,15 @@ const About = () => {
             </p>
           </div>
           <div></div>
-          </div>
-          <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8">
-            <div className="flex justify-center items-center ">
-              <p className="px-4 md:text-md">
-              I'm Yuvaan Pradhan, a Computer Science & Engineering student. I have experience in UI/UX design and web development. I'm driven to learn more technologies in the future and further expand my skills in this field
-
-
-
-
-
-
-
-              </p>
-            
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8">
+          <div className="flex justify-center items-center ">
+            <p className="px-4 md:text-md">
+              I'm Yuvaan Pradhan, a Computer Science & Engineering student. I am
+              a full-stack web developer and have experience in UI/UX design.
+              I'm driven to learn more technologies in the future and further
+              expand my skills in this field
+            </p>
           </div>
           <div className="flex justify-center items-center">
             <img className="rounded-full p-4 md:max-w-sm " src={Me} alt="" />
